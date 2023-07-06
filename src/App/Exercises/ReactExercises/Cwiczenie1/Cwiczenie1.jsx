7403ef54c4df0c6db120d3606d511941b0995d2a
Michał Kukla <michal@kukla.tech>
2023-07-06 18:27:23 +0200

    dodany pusty komponent do ćwiczenia 1

---
+++ src/App/Exercises/ReactExercises/Cwiczenie1/Cwiczenie1.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './style.css';
+
+export function Cwiczenie1() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Formularz został wysłany');
+  }
+
+  return (
+    <div>
+    </div>
+  );
+}
